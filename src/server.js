a0c75269a2f61ca8563242174d0827039d798baf
Ohlavrac <joaovicrcarvalho@gmail.com>
2022-05-14 14:33:42 -0300

    init commit

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const server = express();
+const port = 3000;
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+    res.send("Projeto Qual eh a Boa (By: Debora Cristina, Karlisson Breno, Marcelo Araujo e João Victor Carvalho).");
+});
+
+
+server.listen(port, () => console.log("Server run in port: "+ port));
